5fe610b23216d109b9ef38380c599ce73badbebc
Martin Laurence Coloma <cmartinlaurence@gmail.com>
2024-08-19 00:32:53 +0800

    Project Body started (still ongoing)

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-    console.log('Hello')
+document.getElementById("MyEmail").addEventListener("click", function(event) {
+    event.preventDefault();  // Prevent the default action of the link
+    var spanElement = document.getElementById("SpanEmail");
+    if (spanElement.style.display === "none") {
+        spanElement.style.display = "inline";  // Show the span
+    } else {
+        spanElement.style.display = "none";  // Hide the span
+    }
+});
+
+// Navigation Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
